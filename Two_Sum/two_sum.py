1b4e2588f8fdfa0b7f5c2f192a65f6d059a47a72
HabibaSajid321 <90784842+HabibaSajid321@users.noreply.github.com>
2025-05-05 15:22:50 +0500

    solutions two sum

--- Two_Sum/two_sum.py
+++ Two_Sum/two_sum.py
@@ -1,9 +1,36 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {}  # val -> index
+# Question : Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+# You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+# Return the answer with the smaller index first.
 
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], i]
-            prevMap[n] = i
+# Example 1:
+# Input: 
+# nums = [3,4,5,6], target = 7
+
+# Output: [0,1]
+# Explanation: nums[0] + nums[1] == 7, so we return [0, 1].
+
+# Solution 1: Brute force using nested loops
+
+def two_sum(self,nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i , j]
+    return []
+
+# Time complexity: O(n²)  
+# Space complexity: O(1)
+
+# Solution 2: using hashmap
+
+def twoSum(self, nums, target):
+    hashMap = {}
+
+    for i , n in enumerate(nums):
+        diff = target - n 
+        if diff in hashMap:
+            return [hashMap[diff], i]
+        hashMap[n] = i
+
+# Time complexity: O(n)  
+# Space complexity: O(n)
